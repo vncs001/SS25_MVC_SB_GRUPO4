2a3379993c53246e7244fd7a96ce789658ea63d0
vncs001 <vncsmoraes@gmail.com>
2025-04-17 20:55:31 -0300

    forçando o conflito

--- src/main/java/com/SS25_MVC_SB_GRUPO4/models/Usuario.java
+++ src/main/java/com/SS25_MVC_SB_GRUPO4/models/Usuario.java
@@ -1,5 +1,21 @@
 package main.java.com.SS25_MVC_SB_GRUPO4.models;
+import jakarta.persistence.*;
+import lombok.*;
 
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "usuario")
 public class Usuario {
-    
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String user;
+
+    private String password;
+
+
 }
